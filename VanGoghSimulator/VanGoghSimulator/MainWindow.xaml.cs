888aeb427b78170cdfd9f0cf1cbc6c4c2513ddd1
emilenp <emilpashayev225@gmail.com>
2025-01-20 15:41:31 +0100

    Add files via upload

---
+++ VanGoghSimulator/VanGoghSimulator/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VanGoghSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Color penColor = Color.FromRgb(0, 0, 0);
+        Point currentPoint = new Point();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Image"; // Default file name
+            dialog.DefaultExt = ".png"; // Default file extension
+            dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.PNG;*.JPG;*.GIF|All files (*.*)|*.*";
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void RGBpicker(object sender, RoutedEventArgs e)
+        {
+            Window1 colorpicker = new();
+            colorpicker.ShowDialog();
+
+            if (colorpicker.Success)
+            {
+                penColor = colorpicker.col;
+
+                btn1.Background = new SolidColorBrush(penColor); 
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(this).X;
+                line.Y2 = e.GetPosition(this).Y;
+
+                currentPoint = e.GetPosition(this);
+
+                surface.Children.Add(line);
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(this);
+        }
+    }
+}
---
+++ VanGoghSimulator/VanGoghSimulator/Window1.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VanGoghSimulator
+{
+    /// <summary>
+    /// Interakční logika pro Window1.xaml
+    /// </summary>
+    /// 
+
+    public partial class Window1 : Window
+    {
+        public bool Success;
+
+        public Color col = Color.FromRgb(0, 0, 0);
+        public Window1()
+        {
+            InitializeComponent();
+            rect.Fill = new SolidColorBrush(col);
+        }
+        private void sli_red_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            fillrect((byte)sli_red.Value, "red");
+            txtbox_red.Text = ((byte)sli_red.Value).ToString();
+        }
+
+        private void sli_green_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            fillrect((byte)sli_green.Value, "green");
+            txtbox_green.Text = ((byte)sli_green.Value).ToString();
+        }
+        private void sli_blue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            fillrect((byte)sli_blue.Value, "blue");
+            txtbox_blue.Text = ((byte)sli_blue.Value).ToString();
+        }
+        private void txtbox_red_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtbox_red.Text))
+            {
+                return;
+            }
+            if (byte.TryParse(txtbox_red.Text, out byte val))
+            {
+                sli_red.Value = val;
+                fillrect(val, "red");
+            }
+            else
+            {
+                msgbox();
+                txtbox_red.Text = ((byte)sli_red.Value).ToString();
+            }
+        }
+
+        private void txtbox_green_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtbox_green.Text))
+            {
+                return;
+            }
+            if (byte.TryParse(txtbox_green.Text, out byte val))
+            {
+                sli_green.Value = val;
+                fillrect(val, "green");
+            }
+            else
+            {
+                msgbox();
+                txtbox_green.Text = ((byte)sli_green.Value).ToString();
+            }
+        }
+
+        private void txtbox_blue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtbox_blue.Text))
+            {
+                return;
+            }
+            if (byte.TryParse(txtbox_blue.Text, out byte val))
+            {
+                sli_blue.Value = val;
+                fillrect(val, "blue");
+            }
+            else
+            {
+                msgbox();
+                txtbox_blue.Text = ((byte)sli_blue.Value).ToString();
+            }
+        }
+
+        private void fillrect(byte value, string colourName)
+        {
+            if (value > 255)
+            {
+
+            }
+
+            switch (colourName)
+            {
+                case "red":
+                    col.R = value;
+                    break;
+                case "green":
+                    col.G = value;
+                    break;
+                case "blue":
+                    col.B = value;
+                    break;
+            }
+
+            rect.Fill = new SolidColorBrush(col);
+        }
+
+        private void msgbox()
+        {
+            MessageBox.Show($"Zadejte číslo mezi <0, 255>",
+                            ">:(",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+        }
+
+        private void sucksess(object sender, RoutedEventArgs e)
+        {
+            Success = true;
+            col.R = (byte)sli_red.Value;
+            col.G = (byte)sli_green.Value;
+            col.B = (byte)sli_blue.Value;
+
+            Close();
+        }
+    }
+}
+
